666f099317be4e27fa32b1bf3c0fde84497bcc0f
dev-sanjaykaushal <dev.sanjaykaushal@gmail.com>
2022-05-17 18:58:11 +0530

    Added more settings-lowercase, uppercase, clear, count words and charecters

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -10,18 +10,36 @@ export default function Textform() {
     //On Click function
     const handleClick = ()=>{
         let inputText = document.getElementById('myBox').value;
-        // let inputText = this.state = {value: event.target.value};
         setClickText(inputText);
     }
-
     //On Click function
     const handleOnChange = (event)=>{
         setChangeText(event.target.value);
     }
 
+    //Clear On Click 
+    const handleClear =()=>{
+        setChangeText('');
+        setClickText('');
+    }
+
+    //Change to Lower Case
+    const handleLow =()=>{
+        setChangeText(changeText.toLowerCase());
+        setClickText(clickText.toLowerCase());
+    }
+
+    //Change to Upper Case
+    const handleUpper =()=>{
+        setChangeText(changeText.toUpperCase());
+        setClickText(clickText.toUpperCase());
+    }
+
     //The body of the page to be returned
     return (
+        
         <div className="container">
+            <div className='cards'>
             {/*Grid for changing the label text on button click*/}
             <div className="mb-6 card">
                 <div className="mb-6">
@@ -29,7 +47,9 @@ export default function Textform() {
                     <label className="form-label">{clickText}</label>
                     <input className="form-control" id="myBox" />
                 </div>
-                <button className="btn btn-primary mb-3" onClick={handleClick}>Confirm</button>
+                <button className="btn btn-primary mb-3" onClick={handleClick} >Confirm</button>
+                <h1>Text Summery</h1>
+                <p>{clickText.split(" ").length} words and {clickText.length} charecters</p>
             </div> 
 
             {/*Grid for changing the label text on change*/}
@@ -37,9 +57,18 @@ export default function Textform() {
                 <div className="mb-6">
                     <h2>On Change Functionality</h2>
                     <label className="form-label">{changeText}</label>
-                    <input className="form-control" id="myBox-change" onChange={handleOnChange} />
+                    <input className="form-control" id="myBox-change" onChange={handleOnChange} value={changeText} />
+                    <h1>Text Summery</h1>
+                    <p>{changeText.split(" ").length} words and {changeText.length} charecters</p>
                 </div>
-            </div>   
+            </div>
+            </div>
+            {/* adding global buttons */}
+            <div className='global-buttons'>
+                <button className="btn btn-primary mb-3" onClick={handleClear}>Clear</button>
+                <button className="btn btn-primary mb-3" onClick={handleLow}>Lower Case</button>
+                <button className="btn btn-primary mb-3" onClick={handleUpper}>Upper Case</button>
+            </div>
         </div>
     )
 }
