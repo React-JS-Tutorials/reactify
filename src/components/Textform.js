9077004b869349e60a951b7472360ea0673e6eaf
dev-sanjaykaushal <dev.sanjaykaushal@gmail.com>
2022-05-18 19:13:47 +0530

    Added more tweaks to the app

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -35,6 +35,27 @@ export default function Textform() {
         setClickText(clickText.toUpperCase());
     }
 
+    //Copy clickText
+    const handleClickCopy =()=>{
+        navigator.clipboard.writeText(clickText);
+    }
+
+    //copy clickChange
+    const handleCopyChange =()=>{
+        navigator.clipboard.writeText(changeText);
+    }
+
+    //Remove Extra Spaces
+    const removeSpaces =()=>{  
+        setChangeText(changeText.split(/[ ]+/).join(" "));
+    }
+
+    //Handle Capitalize
+    const handleCapitalize =()=>{  
+        setChangeText(changeText.charAt(0).toUpperCase() + changeText.slice(1));
+        setClickText(clickText.charAt(0).toUpperCase() + clickText.slice(1));
+    }
+
     //The body of the page to be returned
     return (
         
@@ -43,13 +64,16 @@ export default function Textform() {
             {/*Grid for changing the label text on button click*/}
             <div className="mb-6 card">
                 <div className="mb-6">
+                    
                     <h2>On Click Functionality</h2>
                     <label className="form-label">{clickText}</label>
+                    <p>{(clickText === '')?0:clickText.split(" ").length} words and {clickText.length} charecters</p>
                     <input className="form-control" id="myBox" />
                 </div>
-                <button className="btn btn-primary mb-3" onClick={handleClick} >Confirm</button>
-                <h1>Text Summery</h1>
-                <p>{clickText.split(" ").length} words and {clickText.length} charecters</p>
+                <button className="btn btn-primary" onClick={handleClick} >Confirm</button>
+                <button className="btn btn-primary" onClick={handleClickCopy}>Copy</button>
+                
+                
             </div> 
 
             {/*Grid for changing the label text on change*/}
@@ -57,17 +81,19 @@ export default function Textform() {
                 <div className="mb-6">
                     <h2>On Change Functionality</h2>
                     <label className="form-label">{changeText}</label>
+                    <p>{(changeText === '')?0:changeText.split(" ").length} words and {changeText.length} charecters</p>
                     <input className="form-control" id="myBox-change" onChange={handleOnChange} value={changeText} />
-                    <h1>Text Summery</h1>
-                    <p>{changeText.split(" ").length} words and {changeText.length} charecters</p>
+                    <button className="btn btn-primary" onClick={handleCopyChange}>Copy</button>
                 </div>
             </div>
             </div>
             {/* adding global buttons */}
             <div className='global-buttons'>
-                <button className="btn btn-primary mb-3" onClick={handleClear}>Clear</button>
-                <button className="btn btn-primary mb-3" onClick={handleLow}>Lower Case</button>
-                <button className="btn btn-primary mb-3" onClick={handleUpper}>Upper Case</button>
+                <button className="btn btn-primary sm-3" onClick={handleClear}>Clear</button>
+                <button className="btn btn-primary sm-3" onClick={handleLow}>Lower Case</button>
+                <button className="btn btn-primary sm-3" onClick={handleUpper}>Upper Case</button>
+                <button className="btn btn-primary sm-3" onClick={removeSpaces}>Remove Spaces</button>
+                <button className="btn btn-primary sm-3" onClick={handleCapitalize}>Capitalize</button>
             </div>
         </div>
     )
